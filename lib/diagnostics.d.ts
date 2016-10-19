2e6e0da7eb9ea8f3e0427b41c03af719efbf5fca
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-10-19 18:57:57 +0200

    add build script

---
+++ lib/diagnostics.d.ts
@@ -0,0 +1,4 @@
+import gutil = require('gulp-util');
+export declare let PLUGIN_NAME: string;
+export declare function newPluginError(message: string): gutil.PluginError;
+export declare function error(message: string): void;
---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import nodeSwaggerGenerator = require('node-swagger-generator');
+declare function swaggerGenerator(options: nodeSwaggerGenerator.ISwaggerGeneratorOptions): NodeJS.ReadWriteStream;
+export = swaggerGenerator;
